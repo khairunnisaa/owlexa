a7fa062ca73b001676beb79230c3e7788d9effb2
Aditya Subono <adityasubono@gmail.com>
2020-02-04 21:54:04 +0700

    update template

--- src/app/about-owlexa/about-owlexa.component.ts
+++ src/app/about-owlexa/about-owlexa.component.ts
@@ -10,8 +10,14 @@ import {Component, Input, OnInit} from '@angular/core';
   constructor() { }
 
   @Input() judul: any = 'Tentang Owlexa';
-  @Input() smalltext = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit';
-  @Input() description = 'Expedita voluptatum, ratione eveniet velit, distinctio animi voluptas suscipit eos labore voluptatibus?';
+  @Input() smalltext = 'Owlexa sebagai Unit Bisnis';
+  // tslint:disable-next-line:max-line-length
+  @Input() description = 'Untuk memenuhi kebutuhan layanan administrasi asuransi dan jaminan kesehatan yang handal, cepat dan akurat, maka PT Aplikanusa Lintasarta membentuk unit bisnis e-Health dengan brand Owlexa Healthcare. Dengan pengalaman selama 29 tahun sebagai perusahaan yang melayani lebih dari 2500 korporasi, Lintasarta berkomitmen untuk memberikan pelayanan terbaik bagi seluruh stakeholder di industri kesehatan.\n' +
+    '\n' +
+    ' \n' +
+    '\n' +
+    // tslint:disable-next-line:max-line-length
+    'Owlexa memberikan pelayanan tanpa batas, diantaranya memberikan pelayanan terhadap proses administrasi klaim perusahaan atau asuransi mulai dari proses membership & policy administration, eligibility member, case monitoring, discharge sampai pada proses pembayaran tagihan klaim biaya pengobatan. Didukung oleh sistem dan teknologi yang handal serta sumber daya manusia berpengalaman di bidangnya, Owlexa dapat memberikan layanan administrasi kesehatan yang optimal, efisien, cepat dan akurat serta pelayanan terbaik.';
 
   ngOnInit() {
   }
