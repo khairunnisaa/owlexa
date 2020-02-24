5d7720561e904ebd529c33c056dd4c998a74c504
Aditya Subono <adityasubono@gmail.com>
2020-02-24 16:27:51 +0700

    Update Konsultasi Dokter Owlexa

--- src/app/struktur-organisasi/struktur-organisasi.component.ts
+++ src/app/struktur-organisasi/struktur-organisasi.component.ts
@@ -14,126 +14,24 @@ export class StrukturOrganisasiComponent implements OnInit {
       {
         name: "Yosi Widhayanti",
         type: 'General Manager of E-Health Strategic Business Unit',
-        children: [
-          {
-            name: "Hawkeye",
-            type: 'manager',
-            children: []
-          },
-          {
-            name: "Antman",
-            type: 'Manager',
-            children: []
-          }
-        ]
+        children: [ ]
       },
       {
         name: "Tri Rahayu",
         type: 'Operation Manager',
-        children: [
-          {
-            name: "Hulk",
-            type: 'manager',
-            children: [
-              {
-                name: "Spiderman",
-                type: 'Intern',
-                children: []
-              },
-              {
-                name: "Spiderman",
-                type: 'Intern',
-                children: []
-              },
-              {
-                name: "Spiderman",
-                type: 'Intern',
-                children: []
-              },
-              {
-                name: "Spiderman",
-                type: 'Intern',
-                children: []
-              },
-              {
-                name: "Spiderman",
-                type: 'Intern',
-                children: []
-              }
-
-
-            ]
-          },
-          {
-            name: "Purwanti",
-            type: 'SPV',
-            children: [
-              {
-                name: "Loki",
-                type: 'Team Lead',
-                children: []
-              }
-            ]
-          }
-        ]
+        children: [ ]
       },
 
       {
         name: "Angga Priyo Riyanto",
         type: 'Senior Manager IT & Infrastructure',
-        children: [
-          {
-            name: "Hulk",
-            type: 'manager',
-            children: [
-              {
-                name: "Spiderman",
-                type: 'Intern',
-                children: []
-              }
-            ]
-          },
-          {
-            name: "Thor",
-            type: 'Manager',
-            children: [
-              {
-                name: "Loki",
-                type: 'Team Lead',
-                children: []
-              }
-            ]
-          }
-        ]
+        children: []
       },
 
       {
         name: "Elvira Sofiyanti",
         type: 'Provider Relation Manager',
-        children: [
-          {
-            name: "Hulk",
-            type: 'manager',
-            children: [
-              {
-                name: "Spiderman",
-                type: 'Intern',
-                children: []
-              }
-            ]
-          },
-          {
-            name: "Thor",
-            type: 'Manager',
-            children: [
-              {
-                name: "Loki",
-                type: 'Team Lead',
-                children: []
-              }
-            ]
-          }
-        ]
+        children: []
       }
     ]
   };
